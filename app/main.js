766fba9032e14049c3b9de8a8ed0251bd3fe4a7c
Isaac Carvalho <carvalhoisaac.eng+github@gmail.com>
2024-12-26 17:30:15 -0300

    fixing names and locations

--- app/main.js
+++ app/main.js
@@ -39,27 +39,16 @@ function addHTML(item) {
 
     const a = document.createElement('a');
     a.href =
-    item.bloco === "MM1-CF1" ? "index.html?mapa=false" :
-    item.bloco === "MM1-CF2" ? "index.html?mapa=false" :
-    item.bloco === "MM2-G1" ? "plataforma-MM2-G1.html?direct=false" :
-    item.bloco === "MM1-G2" ? "plataforma-MM1-G2.html?direct=false" :
-    item.bloco === "PP2-G2" ? "index.html?mapa=false" :
-    item.bloco === "MM1-G3" ? "plataforma-MM1-G3.html?direct=false" :
-    item.bloco === "PP2-G3" ? "index.html?mapa=false" :
-    item.bloco === "PP4-G3" ? "index.html?mapa=false" :
-    item.bloco === "MM2-G3" ? "plataforma-MM2-G3.html?direct=false" :
-    item.bloco === "PP2-G3-G4" ? "index.html?mapa=false" :
-    item.bloco === "MM1-G4" ? "plataforma-MM1-G4.html?direct=false" :
-    item.bloco === "PP2-G4" ? "index.html?mapa=false" :
-    item.bloco === "MM1-G5" ? "plataforma-MM1-G5.html?direct=false" :
-    item.bloco === "PP2-G5-G6" ? "index.html?mapa=false" :
-    item.bloco === "MM1-G6" ? "plataforma-MM1-G6.html?direct=false" :
-    item.bloco === "MM1-G7" ? "plataforma-MM1-G7.html?direct=false" :
-    item.bloco === "MM1-G8" ? "plataforma-MM1-G8.html?direct=false" :
-    item.bloco === "MM1-G9" ? "plataforma-MM1-G9.html?direct=false" :
-    item.bloco === "MM1-G10" ? "index.html?mapa=false" :
-    item.bloco === "MM1-G11" ? "index.html?mapa=false" :
-    item.bloco === "MM2-PA" ? "index.html?mapa=false" :
+    item.bloco === "MM1-G2" ? "MM1-G2.html?direct=false" :
+    item.bloco === "MM1-G3" ? "MM1-G3.html?direct=false" :
+    item.bloco === "MM1-G4" ? "MM1-G4.html?direct=false" :
+    item.bloco === "MM1-G5" ? "MM1-G5.html?direct=false" :
+    item.bloco === "MM1-G6" ? "MM1-G6.html?direct=false" :
+    item.bloco === "MM1-G7" ? "MM1-G7.html?direct=false" :
+    item.bloco === "MM1-G8" ? "MM1-G8.html?direct=false" :
+    item.bloco === "MM1-G9" ? "MM1-G9.html?direct=false" :
+    item.bloco === "MM2-G1" ? "MM2-G1.html?direct=false" :
+    item.bloco === "MM2-G3" ? "MM2-G3.html?direct=false" :
                                 "index.html?mapa=false"; // Caso padrão para incluir o parâmetro
     a.className = 'content';
 
